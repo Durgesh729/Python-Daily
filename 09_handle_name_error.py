444e458b9aba35297b354f83bfeff3a3dd316693
Durgesh729 <durgeshpadwal729@gmail.com>
2026-06-29 08:49:52 +0000

    Added 09_handle_name_error.py

---
+++ 09_handle_name_error.py
@@ -0,0 +1,26 @@
+# Problem Statement:
+# Write a Python program to use a variable before declaration and handle
+# name errors.
+#
+# Input:
+# None.
+#
+# Output:
+# Variable value or error message.
+try:
+    a=10
+    def f2():
+        b=5
+        return b+d
+    
+    #Or
+
+    def f1():
+        e=5
+        return e
+    f=7
+    g=f+e
+
+    
+except NameError:
+    print("oo")
