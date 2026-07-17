908cc6bb21f3597ccd32b048d2ced777516cb1dc
Durgesh729 <durgeshpadwal729@gmail.com>
2026-07-17 06:08:26 +0000

    Added 20_handle_custom_exception.py

---
+++ 20_handle_custom_exception.py
@@ -0,0 +1,9 @@
+# Problem Statement:
+# Write a Python program to create and handle a custom exception for
+# invalid marks input.
+#
+# Input:
+# Marks value.
+#
+# Output:
+# Valid marks or custom error message.
