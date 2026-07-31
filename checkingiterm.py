ca6179492295ee2183f092b4283992f3aa67f720
Durgesh729 <durgeshpadwal729@gmail.com>
2026-07-31 06:36:37 +0000

    Added checkingiterm.py

---
+++ checkingiterm.py
@@ -0,0 +1,16 @@
+"""
+Problem Statement:
+Check whether every target number is present in a set of available items.
+
+Input: A set of item numbers and a set of target numbers
+Output: True if all targets are present; otherwise False
+Example: Items: 1 2 3, Targets: 1 3 -> Output: True
+"""
+
+# items = set(map(int, input("Enter items: ").split()))
+# target = set(map(int, input("Enter target check that if all present in items: ").split()))
+
+# def function(items, target):
+#     return items.issuperset(target)
+
+# print(function(items, target))
